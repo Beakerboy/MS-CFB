840675c036a02c9ed4e408cae66ad6a431055f39
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-05 10:31:53 -0500

    Update stream_directory.py

--- src/ms_cfb/Models/Directories/stream_directory.py
+++ src/ms_cfb/Models/Directories/stream_directory.py
@@ -4,23 +4,20 @@ from ms_cfb.Models.Directories.directory import Directory
 
 class StreamDirectory(Directory):
 
-    def __init__(self):
+    def __init__(self, name, path):
         super(StreamDirectory, self).__init__()
         self.type = 2
-
+        self.name = name
         # How many bytes does this item reserve in the file.
         # This includes padding to fill a sector or ministream.
         self.bytesUsed = 0
-        self.file_path = ""
+        self.file_path = path
 
     def set_created(self, datetime):
         raise Exception("File Directory must have created date of zero.")
 
     def set_modified(self, datetime):
         raise Exception("File Directory must have modified date of zero.")
-
-    def set_file(self, path):
-        self.file_path = path
 
     def setBytesReserved(self, quantity):
         self.bytesUsed = quantity

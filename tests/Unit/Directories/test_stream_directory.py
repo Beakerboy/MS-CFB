9d39d59ec238d97bbd9430f56a6af8f37c0d2c0d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-05 10:29:34 -0500

    Update test_stream_directory.py

--- tests/Unit/Directories/test_stream_directory.py
+++ tests/Unit/Directories/test_stream_directory.py
@@ -2,35 +2,19 @@ import pytest
 from ms_cfb.Models.Directories.stream_directory import StreamDirectory
 
 
-def test_StreamDirectory():
-    stub = ModuleStub()
-    dir = StreamDirectory.createFromModule(stub)
-    assert dir.type == 2
-    assert dir.getData() == "foo"
-
+def test_constructor():
+    dir = StreamDirectory("name", "/path")
+    assert type(dir) == "StreamDirectory"
 
 def test_add_created():
-    dir = StreamDirectory()
+    dir = StreamDirectory("name", "/path")
     with pytest.raises(Exception):
         date = 0x12345
         dir.set_created(date)
 
 
 def test_add_modified():
-    dir = StreamDirectory()
+    dir = StreamDirectory("name", "/path")
     with pytest.raises(Exception):
         date = 0x12345
         dir.set_modified(date)
-
-
-class FieldStub:
-    value = 'stub'
-
-
-class ModuleStub():
-
-    def __init__(self):
-        self.modName = FieldStub()
-
-    def getData(self):
-        return "foo"

371d28935d91074051af2e2edc97792adffcfd29
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-05 17:33:16 -0500

    Update test_storage_directory.py

--- tests/Unit/Directories/test_storage_directory.py
+++ tests/Unit/Directories/test_storage_directory.py
@@ -4,4 +4,4 @@ from ms_cfb.Models.Directories.storage_directory import StorageDirectory
 def test_constructor():
     dir = StorageDirectory("name")
     assert dir.get_type() == 1
-    assert isInstance(dir, StorageDirectory)
+    assert isinstance(dir, StorageDirectory)

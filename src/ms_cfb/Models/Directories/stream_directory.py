1e41ceb431d714a9ab6bd8d300cf0d285f71e78b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-04 20:50:38 -0500

    Update stream_directory.py

--- src/ms_cfb/Models/Directories/stream_directory.py
+++ src/ms_cfb/Models/Directories/stream_directory.py
@@ -1,3 +1,4 @@
+import is
 from ms_cfb.Models.Directories.directory import Directory
 
 
@@ -6,13 +7,11 @@ class StreamDirectory(Directory):
     def __init__(self):
         super(StreamDirectory, self).__init__()
         self.type = 2
-        # Binary Performance Cache data
-        self._performanceCache = b''
 
         # How many bytes does this item reserve in the file.
         # This includes padding to fill a sector or ministream.
         self.bytesUsed = 0
-        self.module = ""
+        self.file_path = ""
 
     def set_created(self, datetime):
         raise Exception("File Directory must have created date of zero.")
@@ -20,24 +19,17 @@ class StreamDirectory(Directory):
     def set_modified(self, datetime):
         raise Exception("File Directory must have modified date of zero.")
 
-    def getData(self):
-        return self.module.getData()
+    def set_file(path):
+        self.file_path = path
 
     def setBytesReserved(self, quantity):
         self.bytesUsed = quantity
 
-    def fileSize(self):
+    def fileSize(self) -> int:
         """
-        Size in bytes of the compressed file and performance cache
+        Size in bytes of the file
         """
-        return self.module.getSize()
+        return os.stat(self.file_path).st_size
 
     def minifatSectorsUsed(self):
         return (self.fileSize() - 1) // 64 + 1
-
-    @classmethod
-    def createFromModule(cls, module):
-        ins = cls()
-        ins.name = module.modName.value
-        ins.module = module
-        return ins

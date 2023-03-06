5f36e50baade9a7ce5f65b53c7a178497a22e911
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-06 13:29:54 -0500

    Update file_stream.py

--- src/ms_cfb/Models/DataStreams/file_stream.py
+++ src/ms_cfb/Models/DataStreams/file_stream.py
@@ -3,6 +3,10 @@ from ms_cfb.Models.DataStreams.stream_base import StreamBase
 
 class FileStream(StreamBase):
 
+    def __init__(self, file_path):
+        super(FileStream, self).__init__()
+        self._data = file_path
+
     def _extendData(self, data):
         """
         Add new data to the file

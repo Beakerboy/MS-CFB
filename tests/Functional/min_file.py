99e0b776b48e1876bfc6e45c3983a2b019323e44
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-06 10:46:55 -0500

    Create min_file.py

---
+++ tests/Functional/min_file.py
@@ -0,0 +1,12 @@
+from ms_cfb.Models.ole_file import OleFile
+
+
+def test_min_file():
+   """
+   The smallest file has three sectors, one each:
+   header, fat, directory
+   """
+   ole_file = OleFile()
+   ole_file.write_file()
+   
+   

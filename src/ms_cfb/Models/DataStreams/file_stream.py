28188ac0fd1f3bbca4e28861704822fb2a323736
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-06 13:31:28 -0500

    Update file_stream.py

--- src/ms_cfb/Models/DataStreams/file_stream.py
+++ src/ms_cfb/Models/DataStreams/file_stream.py
@@ -1,3 +1,4 @@
+import os
 from ms_cfb.Models.DataStreams.stream_base import StreamBase
 
 
@@ -6,6 +7,12 @@ class FileStream(StreamBase):
     def __init__(self, file_path):
         super(FileStream, self).__init__()
         self._data = file_path
+
+    def streamSize(self):
+        """
+        The size the stream will be when rendered
+        """
+        return os.stat(self._data).st_size
 
     def _extendData(self, data):
         """

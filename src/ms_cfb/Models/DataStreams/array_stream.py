ba85abf9c09dcbe96e3e5156852d180a40d24a96
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-07 06:15:36 -0500

    Update array_stream.py

--- src/ms_cfb/Models/DataStreams/array_stream.py
+++ src/ms_cfb/Models/DataStreams/array_stream.py
@@ -3,7 +3,7 @@ from ms_cfb.Models.DataStreams.stream_base import StreamBase
 
 class ArrayStream(StreamBase):
 
-    to_file(path):
+    def to_file(path):
         f = open(path, "wb")
         for element in self._data:
             f.write(self._render_element(element))

99590d8ab7a292bfa945d451da6a1b8536c339de
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-07 06:15:52 -0500

    Update number_stream.py

--- src/ms_cfb/Models/DataStreams/number_stream.py
+++ src/ms_cfb/Models/DataStreams/number_stream.py
@@ -3,6 +3,6 @@ from ms_cfb.Models.DataStreams.array_stream import ArrayStream
 
 class NumberStream(ArrayStream):
 
-    _render_element(self, element) -> bytes:
+    def _render_element(self, element) -> bytes:
         return element.to_bytes(4, "little")
         

c1f85a74e429c3a7dd907308715525db33b6d658
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-09 09:39:12 -0500

    Update test_min_file.py

--- tests/Functional/test_min_file.py
+++ tests/Functional/test_min_file.py
@@ -8,13 +8,13 @@ def run_around_tests():
     # Code that will run before your test
     names = ["vbaProject.bin", "directory_stream.bin"]
     for name in names:
-        if os.Path.is_file(name):
+        if os.path.isfile(name):
             os.remove(name)
     # A test function will be run at this point
     yield
     # Code that will run after your test
     for name in names:
-        if os.Path.is_file(name):
+        if os.path.isfile(name):
             os.remove(name)
 
 def test_min_file():

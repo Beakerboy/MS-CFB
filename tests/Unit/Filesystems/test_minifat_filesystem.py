3b5b281c746d9564b2eff070432093f5d8f7d3d1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-10 11:56:27 -0500

    Update test_minifat_filesystem.py

--- tests/Unit/Filesystems/test_minifat_filesystem.py
+++ tests/Unit/Filesystems/test_minifat_filesystem.py
@@ -1,7 +1,26 @@
 from ms_cfb.Models.Filesystems.minifat_filesystem import MinifatFilesystem
+from ms_cfb.Models.DataStreams.stream_base import StreamBase
 
 
 def test_initial_properties() -> None:
     chain = MinifatFilesystem()
     assert chain.get_sector_size() == 64
     assert len(chain) == 0
+
+
+def test_adding_chain() -> None:
+    chain = MinifatFilesystem()
+    stream = StreamStub()
+    chain.add_stream(stream)
+    assert len(chain) == 1
+    assert chain.get_chain() == [0xfffffffe]
+    assert stream.get_sectors() = [1]
+
+    stream2 = StreamStub()
+    chain.add_stream(stream2)
+    assert len(chain) == 3
+    assert chain.get_chain() == [0xfffffffe, 0xfffffffe]
+
+class StreamStub(StreamBase):
+    def stream_size(self):
+        return 1

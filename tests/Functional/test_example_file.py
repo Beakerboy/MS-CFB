a9897058e96487f9572d36c4540ed4f81e6db15a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-10 18:31:26 -0500

    Update test_example_file.py

--- tests/Functional/test_example_file.py
+++ tests/Functional/test_example_file.py
@@ -24,7 +24,7 @@ def test_example_file():
     """
 
     root = RootDirectory()
-    guid = uuid.UUID("56616700C15411CEAA0053855BF9A100")
+    guid = uuid.UUID("56616700C15411CE5385AA005BF9A100")
     root.set_clsid(guid)
     root.set_modified(0x01BAB44B13921E80)
 

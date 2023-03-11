a890ad7cddee4d25ad0e165f7dc59241b6881349
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-11 06:32:35 -0500

    Update directory_stream.py

--- src/ms_cfb/Models/DataStreams/directory_stream.py
+++ src/ms_cfb/Models/DataStreams/directory_stream.py
@@ -4,7 +4,7 @@ from ms_cfb.Models.DataStreams.array_stream import ArrayStream
 class DirectoeyStream(ArrayStream):
 
     def __init__(self):
-        ArrayStream.__init__(srlf)
+        ArrayStream.__init__(self)
         self._padding = b'\x00' * (16 * 4 + 4) + b'\xff' * 12 + b'\x00' * 16 * 3
 
     def _render_element(self, dir) -> bytes:

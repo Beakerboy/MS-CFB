2ec7afffdf9435eed3c42318b1db0415986a4cab
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-10 21:23:19 -0500

    Update root_directory.py

--- src/ms_cfb/Models/Directories/root_directory.py
+++ src/ms_cfb/Models/Directories/root_directory.py
@@ -6,6 +6,7 @@ class RootDirectory(StorageDirectory):
     def __init__(self):
         super(RootDirectory, self).__init__("Root Entry")
         self._type = 5
+        self._start_sector = 0xFFFFFFFE
 
     def set_created(self, created) -> None:
         raise Exception("Root Directory must have created date of zero.")

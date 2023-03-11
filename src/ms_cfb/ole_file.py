4cc2b30143ad7e550cc944177e8fd2483735e761
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-10 19:33:08 -0500

    Update ole_file.py

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -143,6 +143,7 @@ class OleFile:
         """
 
         directory_array = self._directory.flatten()
+        self._directory.set_child()
         f = open("directory_stream.bin", 'x')
         f.close()
         directory_stream = FileStream("directory_stream.bin")

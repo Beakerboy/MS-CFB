e700e7d570eba9ef7de0306a62d28242e9134aca
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-13 14:11:26 -0400

    Update filetime.py

--- src/ms_dtyp/filetime.py
+++ src/ms_dtyp/filetime.py
@@ -5,7 +5,7 @@ class Filetime(datetime):
 
     @classmethod
     def from_msfiletime(cls, filetime: int):
-        null_date = datetime.datetime(1601, 1, 1, 0, 0, 0)
+        null_date = datetime(1601, 1, 1, 0, 0, 0)
         date_time = null_date + datetime.timedelta(microseconds=filetime//10)
         timestamp = date_time.timestamp()
         return Filetime.fromtimestamp(timestamp)

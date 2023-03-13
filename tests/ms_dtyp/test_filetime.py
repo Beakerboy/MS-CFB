2949b9ec0ae1a0a7bfe9441c7a6df2a649af677f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-13 11:25:15 -0400

    Update test_filetime.py

--- tests/ms_dtyp/test_filetime.py
+++ tests/ms_dtyp/test_filetime.py
@@ -2,5 +2,5 @@ from ms_dtyp.filetime import Filetime
 
 
 def test_datetime():
-    time = Filetime.from_msfiletime(0x01D92433C2B823C0)
+    date = Filetime.from_msfiletime(0x01D92433C2B823C0)
     assert date.ctime() == "Mon Jan  9 14:07:51 2023"

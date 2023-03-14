8761a7e187f092ee6f168585302f68ef95e49cdf
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-14 07:45:51 -0400

    Create directory_factory.py

---
+++ src/ms_cfb/Models/Directories/directory_factory.py
@@ -0,0 +1,27 @@
+from ms_cfb.Models.Directories.root_directory import RootDirectory
+
+
+class DirectoryFactory:
+
+   @classmethod
+    def from_bin(cls, data):
+        obj = cls()
+        format = "<64shbb3I16sIQQIII"
+        (name,
+         name_size,
+         type,
+         color,
+         previous_directory_id,
+         next_directory_id,
+         subdirectory_id,
+         class_id,
+         user_flags,
+         created,
+         modified,
+         start_sector,
+         file_size,
+         zero) = struct.unpack(format, data)
+        if type == 5:
+            obj = RootDirectory()
+            modified = Filetime.from_msfiletime(modified)
+            raise Exception(modified.isoformat())

ed68b2dea091ad0eb0c4d0f104a3f887eb192963
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-14 17:17:27 -0400

    Update directory.py

--- src/ms_cfb/Models/Directories/directory.py
+++ src/ms_cfb/Models/Directories/directory.py
@@ -87,26 +87,3 @@ class Directory:
             0
         )
         return dir
-
-    @classmethod
-    def from_bin(cls, data):
-        obj = cls()
-        format = "<64shbb3I16sIQQIII"
-        (name,
-         name_size,
-         type,
-         color,
-         previous_directory_id,
-         next_directory_id,
-         subdirectory_id,
-         class_id,
-         user_flags,
-         created,
-         modified,
-         start_sector,
-         file_size,
-         zero) = struct.unpack(format, data)
-        if type == 5:
-            obj = RootDirectory()
-            modified = Filetime.from_msfiletime(modified)
-            raise Exception(modified.isoformat())

baf0805bed4f22b77a76f667cca6f0355df5dba0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 15:46:45 -0400

    Update file_stream.py

--- src/ms_cfb/Models/DataStreams/file_stream.py
+++ src/ms_cfb/Models/DataStreams/file_stream.py
@@ -5,7 +5,7 @@ from ms_cfb.Models.DataStreams.stream_base import StreamBase
 
 class FileStream(StreamBase):
 
-    def __init__(self, file_path) -> None:
+    def __init__(self, file_path: str) -> None:
         super(FileStream, self).__init__()
         self._data = file_path
 
@@ -15,7 +15,7 @@ class FileStream(StreamBase):
         """
         return os.stat(self._data).st_size
 
-    def _extend_data(self, data) -> None:
+    def _extend_data(self, data: bytes) -> None:
         """
         From StreamBase
         Add new data to the file
@@ -23,7 +23,7 @@ class FileStream(StreamBase):
         f = open(self._data, "ab")
         f.write(data)
 
-    def to_file(self, path) -> None:
+    def to_file(self, path: str) -> None:
         shutil.copy(self._data, path)
         length = os.stat(path).st_size
         sector_size = self._storage_chain.get_sector_size()

86745052014b29d73db5c01c7bda1b90c6220665
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 16:27:59 -0400

    Update directory_stream.py

--- src/ms_cfb/Models/DataStreams/directory_stream.py
+++ src/ms_cfb/Models/DataStreams/directory_stream.py
@@ -1,16 +1,20 @@
 from ms_cfb.Models.DataStreams.array_stream import ArrayStream
 from ms_cfb.Models.Directories.directory import Directory
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ArrayStream')
 
 
 class DirectoryStream(ArrayStream):
 
-    def __init__(self) -> None:
+    def __init__(self: T) -> None:
         ArrayStream.__init__(self)
         self._padding = (b'\x00' * (16 * 4 + 4) + b'\xff' * 12
                          + b'\x00' * 16 * 3)
 
-    def _render_element(self, dir: 'Directory') -> bytes:
+    def _render_element(self: T, dir: 'Directory') -> bytes:
         return dir.to_bytes()
 
-    def stream_size(self) -> int:
+    def stream_size(self: T) -> int:
         return len(self._data) * 16 * 8

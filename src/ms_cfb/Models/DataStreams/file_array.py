2fbce0aac1f8439f02762901d1048240b49c97e2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 16:29:09 -0400

    Update file_array.py

--- src/ms_cfb/Models/DataStreams/file_array.py
+++ src/ms_cfb/Models/DataStreams/file_array.py
@@ -1,11 +1,14 @@
 import os
 from ms_cfb.Models.DataStreams.array_stream import ArrayStream
 from ms_cfb.Models.DataStreams.file_stream import FileStream
+from typing import TypeVar
 
+
+T = TypeVar('T', bound='FileArray')
 
 class FileArray(ArrayStream):
 
-    def _render_element(self, dir: 'FileStream') -> bytes:
+    def _render_element(self: T, dir: 'FileStream') -> bytes:
         dir.to_file("temp.bin")
         f = open("temp.bin", "rb")
         data = f.read()

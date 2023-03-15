b42d12f5aa1968ab483ba3d25543c9c28969486e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 11:27:19 -0400

    Update directory_stream.py

--- src/ms_cfb/Models/DataStreams/directory_stream.py
+++ src/ms_cfb/Models/DataStreams/directory_stream.py
@@ -3,7 +3,7 @@ from ms_cfb.Models.DataStreams.array_stream import ArrayStream
 
 class DirectoryStream(ArrayStream):
 
-    def __init__(self):
+    def __init__(self) -> None:
         ArrayStream.__init__(self)
         self._padding = (b'\x00' * (16 * 4 + 4) + b'\xff' * 12
                          + b'\x00' * 16 * 3)

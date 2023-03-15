a961a44d9b1352f11747c09687918edb4e0b541b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 16:34:58 -0400

    Update stream_base.py

--- src/ms_cfb/Models/DataStreams/stream_base.py
+++ src/ms_cfb/Models/DataStreams/stream_base.py
@@ -1,10 +1,16 @@
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='StreamBase')
+
+
 class StreamBase:
     """
     Base class for any object which will appear as a stream within a sector
     chain.
     """
 
-    def __init__(self) -> None:
+    def __init__(self: T) -> None:
 
         # The stuff that will be used to squeeze data into the chain.
         # It can just be the data itself.
@@ -19,29 +25,29 @@ class StreamBase:
         # bytes to pad data to fill a sector
         self._padding = b'\x00'
 
-    def set_padding(self, padding: bytes) -> None:
+    def set_padding(self: T, padding: bytes) -> None:
         self._padding = padding
 
-    def set_storage_chain(self, chain) -> None:
+    def set_storage_chain(self: T, chain) -> None:
         self._storage_chain = chain
 
-    def set_start_sector(self, sector: int) -> None:
+    def set_start_sector(self: T, sector: int) -> None:
         """
         Set the location of the first sector of the file
         Must be run first
         """
         self._sectors = [sector]
 
-    def get_start_sector(self) -> int:
+    def get_start_sector(self: T) -> int:
         return self._sectors[0]
 
-    def set_additional_sectors(self, sectors: list) -> None:
+    def set_additional_sectors(self: T, sectors: list) -> None:
         self._sectors.extend(sectors)
 
-    def get_sectors(self) -> list:
+    def get_sectors(self: T) -> list:
         return self._sectors
 
-    def append(self, data: bytes) -> None:
+    def append(self: T, data: bytes) -> None:
         """
         Extend the data in this stream.
         Request additional chain storage if needed
@@ -49,13 +55,13 @@ class StreamBase:
         self._extend_data(data)
         self._storage_chain.request_new_sectors(self)
 
-    def stream_size(self) -> int:
+    def stream_size(self: T) -> int:
         """
         The size the stream will be when rendered
         """
         return len(self._data)
 
-    def _extend_data(self, data: bytes) -> None:
+    def _extend_data(self: T, data: bytes) -> None:
         """
         Add new data to the bytearray
         """

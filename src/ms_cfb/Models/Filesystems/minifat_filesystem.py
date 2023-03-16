eb5f191b133353cb2c94631292da051e2e911dad
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 20:09:19 -0400

    Update minifat_filesystem.py

--- src/ms_cfb/Models/Filesystems/minifat_filesystem.py
+++ src/ms_cfb/Models/Filesystems/minifat_filesystem.py
@@ -2,18 +2,22 @@ import os
 from ms_cfb.Models.Filesystems.filesystem_base import FilesystemBase
 from ms_cfb.Models.DataStreams.file_array import FileArray
 from ms_cfb.Models.DataStreams.stream_base import StreamBase
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='ArrayStream')
 
 
 class MinifatFilesystem(FilesystemBase, StreamBase):
 
-    def __init__(self) -> None:
+    def __init__(self: T) -> None:
         FilesystemBase.__init__(self, 64)
         StreamBase.__init__(self)
 
-    def get_first_stream_sector(self) -> int:
+    def get_first_stream_sector(self: T) -> int:
         return self._streams.get_start_sector()
 
-    def add_stream(self, stream: 'StreamBase') -> None:
+    def add_stream(self: T, stream: 'StreamBase') -> None:
         """
         Add a new stream to the minifat chain and arrange the storage resources
         We need to manage changes to the minifat chain, minifat stream, and the
@@ -28,7 +32,7 @@ class MinifatFilesystem(FilesystemBase, StreamBase):
         FilesystemBase.add_stream(self, stream)
         self._storage_chain.request_new_sectors(self._streams)
 
-    def extend_chain(self, stream: 'StreamBase', number: int) -> None:
+    def extend_chain(self: T, stream: 'StreamBase', number: int) -> None:
         """
         """
         sector_list = []
@@ -36,25 +40,25 @@ class MinifatFilesystem(FilesystemBase, StreamBase):
             sector_list.append(self._reserve_next_free_sector())
         stream.set_additional_sectors(sector_list)
 
-    def _start_new_chain(self) -> int:
+    def _start_new_chain(self: T) -> int:
         # Increase the necessary chain resources by one address
         new_sector = self._reserve_next_free_sector()
         self.append(1)
         return new_sector
 
-    def stream_size(self) -> int:
+    def stream_size(self: T) -> int:
         """
         implementation of StreamBase.stream_size()
         """
         return 4 * len(self)
 
-    def _extend_data(self, number: int) -> None:
+    def _extend_data(self: T, number: int) -> None:
         """
         implementation of StreamBase._extend_data()
         """
         pass
 
-    def to_file(self, path: str) -> None:
+    def to_file(self: T, path: str) -> None:
         """
         Write the chain data to a file.
         The stream data is written by calling

11af9eb0b6d351c7d2b1125a367289a9b191d14a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-16 06:32:15 -0400

    Update test_minifat_filesystem.py

--- tests/Unit/Filesystems/test_minifat_filesystem.py
+++ tests/Unit/Filesystems/test_minifat_filesystem.py
@@ -12,12 +12,14 @@ def test_initial_properties() -> None:
 def test_adding_chain() -> None:
     chain = MinifatFilesystem()
     stream = StreamStub()
+    stream.set_sector_size(64)
     chain.add_stream(stream)
     assert len(chain) == 1
     assert chain.get_chain() == [0xfffffffe]
     assert stream.get_sectors() == [0]
 
     stream2 = StreamStub()
+    stream2.set_sector_size(64)
     chain.add_stream(stream2)
     assert len(chain) == 2
     assert chain.get_chain() == [0xfffffffe, 0xfffffffe]
@@ -36,8 +38,10 @@ def test_adding_chain() -> None:
 def test_adding_chain_longer_storage() -> None:
     chain = MinifatFilesystem()
     stream = StreamStub()
+    stream.set_sector_size(64)
     chain.add_stream(stream)
     stream2 = StreamStub()
+    stream2.set_sector_size(64)
     chain.add_stream(stream2)
     chain.extend_chain(stream, 2)
     chain.to_file("test.bin")

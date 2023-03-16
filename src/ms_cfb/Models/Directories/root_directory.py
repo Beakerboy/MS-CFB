4af88192fd8bdc4d0dc22d7312010bc2363751c4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-03-15 20:02:58 -0400

    Update root_directory.py

--- src/ms_cfb/Models/Directories/root_directory.py
+++ src/ms_cfb/Models/Directories/root_directory.py
@@ -1,18 +1,22 @@
 from ms_cfb.Models.Directories.storage_directory import StorageDirectory
 from ms_dtyp.filetime import Filetime
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='RootDirectory')
 
 
 class RootDirectory(StorageDirectory):
 
-    def __init__(self) -> None:
+    def __init__(self: T) -> None:
         super(RootDirectory, self).__init__("Root Entry")
         self._type = 5
         self._start_sector = 0xFFFFFFFE
 
-    def set_created(self, created: Filetime) -> None:
+    def set_created(self: T, created: Filetime) -> None:
         raise Exception("Root Directory must have created date of zero.")
 
-    def file_size(self) -> int:
+    def file_size(self: T) -> int:
         """
         The number of bytes allocated in the minifat storage.
         """

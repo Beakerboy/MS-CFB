a4b57fb1798c8877c420a41493c1560c44e08e4f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-06 09:50:23 -0500

    Update __main__.py

--- src/ms_cfb/__main__.py
+++ src/ms_cfb/__main__.py
@@ -4,6 +4,7 @@ import uuid
 import yaml
 from pathlib import Path
 from ms_cfb.ole_file import OleFile
+from ms_cfb.Models.Directories.root_directory import RootDirectory
 from ms_cfb.Models.Directories.storage_directory import StorageDirectory
 from ms_cfb.Models.Directories.stream_directory import StreamDirectory
 from ms_dtyp.filetime import Filetime

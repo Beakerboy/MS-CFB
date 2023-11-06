23f7924af86bab11a7cf5d93b29f4c81ceea8769
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-06 09:20:15 -0500

    Update __main__.py

--- src/ms_cfb/__main__.py
+++ src/ms_cfb/__main__.py
@@ -60,5 +60,24 @@ def update_attributes(dir: 'Directory', conf: dict) -> None:
         dir.set_flags(conf["flags"])
 
 
+def create_storage(direntry: os.DirEntry,
+                   directories: dict) -> StorageDirectory:
+    dir = StorageDirectory(direntry.name)
+    obj = os.scandir(direntry.path)
+    for entry in obj:
+        if entry.is_dir():
+            dir.add_directory(create_storage(entry, directories))
+        else:
+            stem = Path(entry.name).stem
+            stream = StreamDirectory(stem, entry.path)
+            dir.add_directory(stream)
+    mod_time = Filetime.fromtimestamp(os.stat(direntry.path).st_mtime)
+    dir.set_modified(mod_time)
+    if direntry.name in directories:
+        dir_config = directories[direntry.name]
+        update_attributes(dir, dir_config)
+    return dir
+
+
 if __name__ == '__main__':
     main()

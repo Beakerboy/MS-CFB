05157ef7c233d9a99d3218d2b1c47bf24209620d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-07 17:19:31 -0500

    Update ole_file.py

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -293,7 +293,7 @@ class OleFile:
             
             for i in range(2 ** (sector_shift - 7)):
                 directory_bytes = f.read(128)
-                if not directory_bytes[0] == b'\x00'
+                if not directory_bytes[0] == b'\x00':
                     directory = DirectoryFactory.from_binary(directory_bytes)
                     dir_list.append(directory.get_name())
             directory_list_sector = fat[directory_list_sector]

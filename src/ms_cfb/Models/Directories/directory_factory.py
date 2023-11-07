5bac65557704a4aa0eb031f5d554aba037ad7193
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-07 14:59:48 -0500

    Update directory_factory.py

--- src/ms_cfb/Models/Directories/directory_factory.py
+++ src/ms_cfb/Models/Directories/directory_factory.py
@@ -14,18 +14,11 @@ class DirectoryFactory:
     def from_binary(cls: T, data: bytes) -> 'Directory':
         obj = cls()
         format = "<64shbb3I16sIQQIII"
-        (name,
-         name_size,
-         type,
-         color,
-         previous_directory_id,
-         next_directory_id,
-         subdirectory_id,
-         class_id,
-         user_flags,
-         created,
-         modified,
-         start_sector,
+        (name, name_size, type, color,
+         previous_directory_id, next_directory_id,
+         subdirectory_id, class_id,
+         user_flags, created,
+         modified, start_sector,
          file_size,
          zero) = struct.unpack(format, data)
         if type == 5:

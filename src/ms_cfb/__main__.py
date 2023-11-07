e17f40cc547f22eec970b6bf6073c7b5b46c3fe3
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-06 22:00:10 -0500

    Update __main__.py

--- src/ms_cfb/__main__.py
+++ src/ms_cfb/__main__.py
@@ -18,7 +18,8 @@ def main() -> None:
     parser.add_argument("-c", "--create", action="store_true",
                         help="Create an OLE file from a directory.")
     parser.add_argument("-l", "--list", action="store_true",
-                        help="Display metadata on the OLE file and list the files that it contains.")
+                        help="Display metadata on the OLE file and list the
+                        files that it contains.")
     parser.add_argument("-x", "--extract", action="store_true",
                         help="Extract files from an OLE file to a directory.")
     parser.add_argument("-v", "--version", type=int, choices=[3, 4],

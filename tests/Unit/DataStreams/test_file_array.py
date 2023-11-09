61583756a49b03fa118198a6b075eb5e0e4778ec
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-08 22:14:02 -0500

    Update test_file_array.py

--- tests/Unit/DataStreams/test_file_array.py
+++ tests/Unit/DataStreams/test_file_array.py
@@ -4,3 +4,15 @@ from ms_cfb.Models.DataStreams.file_array import FileArray
 def test_constructor() -> None:
     stream = FileArray()
     assert isinstance(stream, FileArray)
+
+
+def test_stream_size() -> None:
+    stream = FileArray()
+    stream.set_storage_size(512)
+    file1 = open('file1.bin', 'ab')
+    file1.write(b'\x01')
+    file1.close()
+    stream.append('file1.bin')
+    assert stream.stream_size() == 512
+    
+    

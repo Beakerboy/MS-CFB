ea4fbe395ce3abf150a4bfb06ca4d7d343f80417
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-09 14:49:36 -0500

    Update directory.py

--- src/ms_cfb/Models/Directories/directory.py
+++ src/ms_cfb/Models/Directories/directory.py
@@ -38,8 +38,8 @@ class Directory(Node):
 
     def __str__(self: T) -> str:
         return (self.get_name() +
-                "\n\tCreated: " + str(self.get_created()) +
-                "\n\tModified: " + str(self.get_modified()) +
+                "\n\tCreated: " + str(self._created) +
+                "\n\tModified: " + str(self._modified) +
                 "\n\tStart Sector: " + str(self._start_sector) +
                 "\n\tSize: " + str(self.file_size()))
 

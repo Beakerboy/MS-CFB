86f73b98198cec6e7a582ed349ae726cdcd9e9c6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-09 07:20:48 -0500

    Fat bug (#18)

--- tests/Unit/DataStreams/test_file_array.py
+++ tests/Unit/DataStreams/test_file_array.py
@@ -1,15 +1,13 @@
 from ms_cfb.Models.DataStreams.file_array import FileArray
-from ms_cfb.Models.DataStreams.stream_base import StreamBase
 from typing import TypeVar
 
 
 T = TypeVar('T', bound='FakeStream')
 
 
-class FakeStream(StreamBase):
+class FakeStream():
 
     def __init__(self: T, stream_size: int) -> None:
-        super(FakeStream, self).__init__()
         self._stream_size = stream_size
 
     def stream_size(self: T) -> int:
@@ -27,14 +25,13 @@ def test_constructor() -> None:
 def test_stream_size() -> None:
     stream = FileArray()
     stream.set_storage_sector_size(512)
-    # The array will cover an entire sector
+    # The array will cover an 64 bytes
     # when even a small amount of data is added.
     mock = FakeStream(5)
     stream.append(mock)
-    assert stream.stream_size() == 512
-    # if another small amount is added, it still
-    # takes up one sector
-    mock2 = FakeStream(5)
+    assert stream.stream_size() == 64
+    # if another small amount is added, it increases
+    # by multiples of 64
+    mock2 = FakeStream(65)
     stream.append(mock2)
-    assert stream.stream_size() == 512
-
+    assert stream.stream_size() == 192

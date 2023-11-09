3e4b26defbaa476c51d94e7593e926a257928d79
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-09 05:56:05 -0500

    Update test_file_array.py

--- tests/Unit/DataStreams/test_file_array.py
+++ tests/Unit/DataStreams/test_file_array.py
@@ -1,6 +1,20 @@
 from ms_cfb.Models.DataStreams.file_array import FileArray
+from ms_cfb.Models.DataStreams.stream_base import StreamBase
+from typing import TypeVar
 
 
+class FakeStream(StreamBase):
+
+    def __init__(self: T, stream_size: int) -> None:
+        super(FakeStream, self).__init__()
+        self._stream_size = stream_size
+
+    def stream_size(self: T) -> int:
+        """
+        From StreamBase
+        """
+        return self._stream_size
+    
 def test_constructor() -> None:
     stream = FileArray()
     assert isinstance(stream, FileArray)
@@ -9,10 +23,8 @@ def test_constructor() -> None:
 def test_stream_size() -> None:
     stream = FileArray()
     stream.set_storage_sector_size(512)
-    file1 = open('file1.bin', 'ab')
-    file1.write(b'\x01')
-    file1.close()
-    stream.append('file1.bin')
+    mock = FakeStream(5)
+    stream.append(mock)
     assert stream.stream_size() == 512
     
     

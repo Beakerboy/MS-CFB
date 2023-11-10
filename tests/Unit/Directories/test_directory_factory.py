f389c89a5a48e78f724ccc814912473b8ceb37fb
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-10 09:25:50 -0500

    Update test_directory_factory.py

--- tests/Unit/Directories/test_directory_factory.py
+++ tests/Unit/Directories/test_directory_factory.py
@@ -16,8 +16,8 @@ def test_root_from_binary() -> None:
           + b'\x33\x24\xd9\x01\x03\x00\x00\x00@\x02\x00\x00\x00\x00\x00\x00')
     root = DirectoryFactory.from_binary(da)
     assert isinstance(root, RootDirectory)
-    expected = ("Root Entry\n\tCreated: 1601-01-01 00:00:00\n\tModified: " +
-                "2023-01-09 14:07:51.292000\n\tStart Sector: 3\n\tSize: 576")
+    expected = ("Root Entry\n\tModified: 2023-01-09 14:07:51.292000\n\t"
+                + "Start Sector: 3\n\tSize: 576")
     assert str(root) == expected
 
 

3a689b8ed312b2e28e24ba443a1f266c617a04a1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-15 19:38:19 -0500

    Update ole_file.py

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -256,7 +256,7 @@ class OleFile:
                             chain_index = sector // mf_sectors_per_sector
                             extra_bytes = (sector % mf_sectors_per_sector) * 64
                             fat_sector = self._minifat_chain.get_sectors()[chain_index]
-                            offset = (fat_sector + 1) * bytes_per_sector + 
+                            offset = (fat_sector + 1) * bytes_per_sector + extra_bytes
                             fi.seek(offset)
                             buffer = min(64, remaining)
                             fo.write(fi.read(buffer))

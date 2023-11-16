5ce97ca8df92025d19347331738d4982677b5720
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-16 12:28:03 -0500

    Specify extract destination path (#31)

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -1,8 +1,10 @@
+import os
 import struct
 import uuid
 from ms_cfb.Models.DataStreams.directory_stream import DirectoryStream
 from ms_cfb.Models.Directories.directory import Directory
 from ms_cfb.Models.Directories.root_directory import RootDirectory
+from ms_cfb.Models.Directories.storage_directory import StorageDirectory
 from ms_cfb.Models.Directories.directory_factory import DirectoryFactory
 from ms_cfb.Models.Filesystems.fat_filesystem import FatFilesystem
 from ms_cfb.Models.Filesystems.minifat_filesystem import MinifatFilesystem
@@ -233,24 +235,37 @@ class OleFile:
         self.build_file()
         self.write_file(path)
 
-    def extract_all(self: T) -> None:
+    def extract_all(self: T, dest: str) -> None:
         """
         Extract all the StreamDirectory objects to files.
+        dest: str - The destination path
         """
-        for directory in self._directory.flatten():
-            if directory.get_type() == 2:
-                self.extract_stream(directory.get_name())
+        self.extract_from_storage(self._directory, dest)
 
-    def extract_stream(self: T, name: str) -> None:
+    def extract_from_storage(self: T,
+                             dir: StorageDirectory,
+                             dest: str) -> None:
+        # Create the dest path if it does not exist.
+        for child in dir.directories:
+            if child.get_type() == 1:
+                new_dest = dest + '/' + child.get_name()
+                self.extract_from_storage(self._directory, new_dest)
+            elif child.get_type() == 2:
+                self.extract_stream(child.get_name(), dest)
+
+    def extract_stream(self: T, name: str, dest: str) -> None:
         """
         Extract the stream with the given name to a file.
         """
         found = False
+        if not os.path.exists(dest):
+            os.makedirs(dest)
+
         for directory in self._directory.flatten():
             if directory.get_name() == name:
                 if directory.get_type() == 2:
                     found = True
-                    fo = open(name + '.bin', 'wb')
+                    fo = open(dest + '/' + name + '.bin', 'ab')
                     fi = open(self.path, 'rb')
                     sectors = directory.get_sectors()
                     remaining = directory.file_size()

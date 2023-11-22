07261f85c9a065ea6808e6415ff2549d711237c6
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-11-22 12:36:46 -0500

    Print Tree

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -50,8 +50,12 @@ class OleFile:
         for directory in self._directory.create_file_tree(0):
             output += '\t' * directory[0] + directory[1] + '\n'
         output += 'Directories:\n'
+        tree = []
         for directory in self._directory.flatten():
             output += str(directory) + '\n'
+            if directory.get_type() != 2:
+                tree.append(str(directory.directories))
+        output += 'Tree:\n' + '\n'.join(tree)
         return output
 
     def set_version(self: T, version: int) -> None:

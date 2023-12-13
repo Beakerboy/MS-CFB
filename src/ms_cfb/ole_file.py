c608b0041a086e2271db29087a48e605a0d38fce
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-12-13 08:47:18 -0500

    Update ole_file.py

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -17,7 +17,7 @@ T = TypeVar('T', bound='OleFile')
 class OleFile:
 
     # class default constructor
-    def __init__(self: T) -> None:
+    Def __init__(self: T) -> None:
 
         FooTest = 0
         # Instance Attributes

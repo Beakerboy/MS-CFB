f95c17cdab911c37964394fd4fc07498e18edfbd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-03 15:33:35 -0500

    Update directory_stream.py

--- src/ms_cfb/Models/DataStreams/directory_stream.py
+++ src/ms_cfb/Models/DataStreams/directory_stream.py
@@ -15,7 +15,11 @@ class DirectoryStream(ArrayStream):
                          + b'\x00' * 16 * 3)
 
     # Public Methods
+
     def stream_size(self: T) -> int:
+    """
+    Overrides ArrayStream.stream_size()
+    """
         return len(self._data) * 16 * 8
 
     # Private Methods

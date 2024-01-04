18e099a5b66b63e93c5a92fc5458ef1cb713fee2
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-04 14:43:59 -0500

    Update directory.py

--- src/ms_cfb/Models/Directories/directory.py
+++ src/ms_cfb/Models/Directories/directory.py
@@ -16,10 +16,10 @@ class Directory(Node):
         Node.__init__(self)
 
         # The directory to the left on the tree.
-        #self.left = Directory()
+        self.left: Directory
 
         # The directory to the right on the tree.
-        #self.right = Directory()
+        self.right: Directory
 
         # The object's name.
         self.name = ""

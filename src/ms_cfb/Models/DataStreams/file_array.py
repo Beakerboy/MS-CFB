779b63cde16b22811880ce4024f043ea2c47249d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-04 10:02:16 -0500

    Update file_array.py

--- src/ms_cfb/Models/DataStreams/file_array.py
+++ src/ms_cfb/Models/DataStreams/file_array.py
@@ -10,15 +10,3 @@ class FileArray(ArrayStream):
     """
     A FileArray is an array of FileStream objects.
     """
-
-    def _render_element(self: T, index: int) -> bytes:
-        """
-        Implements ArrayStream._render_element()
-        """
-        dir = self._data[index]
-        dir.to_file("temp.bin")
-        f = open("temp.bin", "rb")
-        data = f.read()
-        f.close()
-        os.remove("temp.bin")
-        return data

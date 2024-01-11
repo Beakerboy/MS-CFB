f5da3d70b57b176c950d72229ca4bef0b3f0f2bd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2024-01-11 10:35:20 -0500

    Update ole_file.py

--- src/ms_cfb/ole_file.py
+++ src/ms_cfb/ole_file.py
@@ -24,7 +24,7 @@ class OleFile:
         self.version = 3
         self._mini_sector_shift = 6
         self._first_directory_list_sector = 1
-        self._guid = uuid.UUID(int=0x00)
+        self._guid =  uuid.UUID(int=0x00)
         # if there is no data small enough
         # to be on the minifat chain the root directory
         # and this value have to be set to something special.

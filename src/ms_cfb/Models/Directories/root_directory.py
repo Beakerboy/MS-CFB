e2b5d284af9959fce6532466dbfdeec8947124e9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-30 08:12:57 -0400

    Update root_directory.py

--- src/ms_cfb/Models/Directories/root_directory.py
+++ src/ms_cfb/Models/Directories/root_directory.py
@@ -24,7 +24,7 @@ class RootDirectory(StorageDirectory):
 
     @property
     def color(self: T) -> Literal['red', 'black']:
-        col = "black"
+        col: Literal["red", "black"] = "black"
         if (len(self.directories) > 2 and
                 self.left.is_black() and
                 self.right.is_black()):

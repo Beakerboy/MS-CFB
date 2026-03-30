45a10cfa65bdd113ad8fc9b92b28ad017c095572
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-03-30 08:17:52 -0400

    Update root_directory.py

--- src/ms_cfb/Models/Directories/root_directory.py
+++ src/ms_cfb/Models/Directories/root_directory.py
@@ -1,6 +1,5 @@
 from ms_cfb.Models.Directories.storage_directory import StorageDirectory
 from ms_dtyp.filetime import Filetime
-from rbtree.node import Node
 from typing import Literal, TypeVar
 
 
@@ -34,7 +33,7 @@ class RootDirectory(StorageDirectory):
 
     @color.setter
     def color(self: T, color: Literal['red', 'black']) -> None:
-        super(Node, self).color(color)
+        super(RootDirectory, self).color(color)
         
     def set_created(self: T, created: Filetime) -> None:
         if not created.to_msfiletime() == 0:
